97bff65146e5bf5b45ef1e27d4f90e2864ed4b79
lumusitec <lumusitec@gmail.com>
2021-08-13 11:08:36 -0300

    [ADD] repositories added

--- src/startup/container.js
+++ src/startup/container.js
@@ -17,6 +17,9 @@ const Routes = require('../routes')
 // Models
 const { User, Idea, Comment } = require('../models')
 
+// Repositories
+const { UserRepository, IdeaRepository, CommentRepository } = require('../repositories')
+
 const container = createContainer()
 
 container
@@ -36,6 +39,10 @@ container
         User: asValue(User),
         Idea: asValue(Idea),
         Comment: asValue(Comment)
+    }).register({
+        UserRepository: asClass(UserRepository).singleton(),
+        IdeaRespository: asClass(IdeaRepository).singleton(),
+        CommentRepository: asClass(CommentRepository).singleton()
     })
 
 module.exports = container
